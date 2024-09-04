d99413f6f9c3008b18ff54fde4f04ea9af751fc6
okituibrian <okituibrian@gmail.com>
2024-09-04 16:45:04 +0300

    feat: added auth repo

---
+++ e-pharmacy/app/src/main/java/com/example/e_dawapharmacy/repository/AuthRepository.kt
@@ -0,0 +1,25 @@
+package com.example.e_dawapharmacy.repository
+
+import com.example.e_dawapharmacy.data.ValidateEmailBody
+import com.example.e_dawapharmacy.utils.APIConsumer
+import com.example.e_dawapharmacy.utils.RequestStatus
+import com.example.e_dawapharmacy.utils.SimplifiedMessage
+import kotlinx.coroutines.flow.flow
+
+class AuthRepository(private val consumer: APIConsumer) {
+    fun validateEmailAddress(body: ValidateEmailBody) = flow {
+        emit(RequestStatus.Waiting)
+        val response = consumer.validateEmailAddress(body)
+        if (response.isSuccessful) {
+            emit(RequestStatus.Success(response.body()!!))
+        } else {
+            emit(
+                RequestStatus.Error(
+                    SimplifiedMessage.get(
+                        response.errorBody()!!.byteStream().reader().readText()
+                    )
+                )
+            )
+        }
+    }
+}
