21248a8a153ed4c9ba7158821764091c6cdc883f
okituibrian <okituibrian@gmail.com>
2024-09-04 16:44:52 +0300

    feat: created api service get function

--- e-pharmacy/app/src/main/java/com/example/e_dawapharmacy/utils/APIService.kt
+++ e-pharmacy/app/src/main/java/com/example/e_dawapharmacy/utils/APIService.kt
@@ -1,8 +1,26 @@
 package com.example.e_dawapharmacy.utils
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 object APIService {
     private const val BASE_URL = "base_url"
 
-  /*  fun getService(): APIConsumer{
-    }*/
+    fun getService(): APIConsumer {
+
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .build()
+
+        val builder: Retrofit.Builder = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+        val retrofit: Retrofit = builder.build()
+        return retrofit.create(APIConsumer::class.java)
+    }
 }
