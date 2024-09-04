15be4de709fb7481c59be6f8917217d4609b3da4
okituibrian <okituibrian@gmail.com>
2024-09-04 16:44:31 +0300

    feat: created api consumer

--- e-pharmacy/app/src/main/java/com/example/e_dawapharmacy/utils/APIConsumer.kt
+++ e-pharmacy/app/src/main/java/com/example/e_dawapharmacy/utils/APIConsumer.kt
@@ -1,4 +1,14 @@
 package com.example.e_dawapharmacy.utils
 
+import com.example.e_dawapharmacy.data.UniqueEmailValidationResponse
+import com.example.e_dawapharmacy.data.ValidateEmailBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface APIConsumer {
+
+    @POST()
+    fun validateEmailAddress(@Body body: ValidateEmailBody): Response<UniqueEmailValidationResponse>
+
 }
