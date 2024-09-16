6a831b9aacd39d34a847e86eb595b022759b4bbb
okituibrian <okituibrian@gmail.com>
2024-09-16 16:28:14 +0300

    fix: sorted issues

--- e-pharmacy/app/src/main/java/com/example/e_dawapharmacy/test3.kt
+++ e-pharmacy/app/src/main/java/com/example/e_dawapharmacy/test3.kt
@@ -1,5 +1,28 @@
 package com.example.e_dawapharmacy
 
 fun main(){
-    println("hello brian")
+    val updatedNames  = simpleArrayNames(arrayOf("james", "anne", "mike"))
+
+    println(updatedNames)
 }
+
+fun simpleArrayNames(array: Array<String>):String{
+    var employees = ""
+    for (name in array){
+       if (name == "james"){
+           employees += "john\n"
+       }else{
+           employees += "$name\n"
+       }
+    }
+    return employees
+}
+
+/*fun simpleArraySum(array: Array<Int>): Int{
+    var sum = 0
+    for (element in array){
+        sum += element
+    }
+    return sum
+}*/
+
