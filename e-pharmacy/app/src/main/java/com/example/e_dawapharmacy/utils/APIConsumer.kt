1b879bfcb36b6fdc9bafca673dc1787d6fe97897
okituibrian <okituibrian@gmail.com>
2024-09-17 16:46:25 +0300

    feat: base url

--- e-pharmacy/app/src/main/java/com/example/e_dawapharmacy/utils/APIConsumer.kt
+++ e-pharmacy/app/src/main/java/com/example/e_dawapharmacy/utils/APIConsumer.kt
@@ -1,5 +1,7 @@
 package com.example.e_dawapharmacy.utils
 
+import com.example.e_dawapharmacy.data.RegisterBody
+import com.example.e_dawapharmacy.data.RegisterResponse
 import com.example.e_dawapharmacy.data.UniqueEmailValidationResponse
 import com.example.e_dawapharmacy.data.ValidateEmailBody
 import retrofit2.Response
@@ -11,4 +13,7 @@ interface APIConsumer {
    /* @POST()
     fun validateEmailAddress(@Body body: ValidateEmailBody): Response<UniqueEmailValidationResponse>
 */
+
+    @POST("demo/add-user")
+    suspend fun registerUser(@Body body: RegisterBody): Response<RegisterResponse>
 }
